c0cee2404f426df407670b27d608e5479b9a7706
Muhammed Deniz <87073177+DenizMuhammed@users.noreply.github.com>
2024-09-28 23:11:10 +0300

    Update tipler.ts

--- tipler.ts
+++ tipler.ts
@@ -15,3 +15,21 @@ eleman2 : number,
 eleman1 : 'deneme',
 eleman2 : 43
 };
+
+// Çok tipli dizi
+const dizi : (string, number, boolean) [] = [];
+
+const dizi2 = [] as Array<string>; //
+
+// yeni tip tanımlama (type aliasing):
+type Colors = 'blue' | 'red' | 'orange';
+
+type Okul = {
+  sinif : string[],
+  seviye: string,
+  nufus: number
+}
+
+// Tipleri birleştirme:
+type ColorsOkul = Colors & Okul;
+
